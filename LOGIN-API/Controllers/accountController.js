ff6245ecfc5834b2ca32f307ee041abffbda8ddb
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-14 16:08:08 -0500

    Update accountController.js
    finalizes handling all parts of a user's needed request

--- LOGIN-API/Controllers/accountController.js
+++ LOGIN-API/Controllers/accountController.js
@@ -43,3 +43,34 @@ exports.view = function (req, res) {
         });
     });
 };
+// Handle update user info
+exports.update = function (req, res) {
+account.findById(req.params.account_id, function (err, user) {
+        if (err)
+            res.send(err);
+            user.username = req.body.username ? req.body.username : user.username;
+            user.password = req.body.password ? req.body.password : user.password;
+// save the user and check for errors
+        user.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Account Info updated',
+                data: user
+            });
+        });
+    });
+};
+// Handle delete user
+exports.delete = function (req, res) {
+    account.remove({
+        _id: req.params.account_id
+    }, function (err, user) {
+        if (err)
+            res.send(err);
+        res.json({
+            status: "success",
+            message: 'Account deleted'
+        });
+    });
+};
