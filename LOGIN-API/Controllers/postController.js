3df419014f27bfa2654cb63ca7a27fd43c14a5cf
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-14 16:19:13 -0500

    Update postController.js

--- LOGIN-API/Controllers/postController.js
+++ LOGIN-API/Controllers/postController.js
@@ -16,3 +16,20 @@ exports.index = function (req, res) {
         });
     });
 };
+// Handle create user actions
+exports.new = function (req, res) {
+    var user = new post();
+    user.username = req.body.username ? req.body.username : user.username;
+    user.content = req.body.content;
+    user.pfp=req.body.pfp;
+// save the user and check for errors
+    user.save(function (err) {
+        // if (err)
+        //     res.json(err);
+        res.json({
+            message: 'New post created!',
+            data: user
+        });
+    });
+};
+
