2089def188da9666a95c621ab1d51b248160312a
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 15:57:45 -0500

    Update accountController.js
    add final authenticative tokens

--- LOGIN-API/Controllers/accountController.js
+++ LOGIN-API/Controllers/accountController.js
@@ -22,17 +22,29 @@ exports.register = async (req, res, next) => {
     })
 }
 
-  bcrypt.hash(password, 10).then(async (hash) => {
+   bcrypt.hash(password, 10).then(async (hash) => {
     await User.create({
       username,
       password: hash,
     })
-      .then((user) =>
-        res.status(200).json({
+      .then((user) => {
+        const maxAge = 3 * 60 * 60;
+        const token = jwt.sign(
+          { id: user._id, username, role: user.role },
+          jwtSecret,
+          {
+            expiresIn: maxAge, // 3hrs in sec
+          }
+        );
+        res.cookie("jwt", token, {
+          httpOnly: true,
+          maxAge: maxAge * 1000, // 3hrs in ms
+        });
+        res.status(201).json({
           message: "User successfully created",
-           user,
-        })
-      )
+          user: user._id,
+        });
+      })
       .catch((error) =>
         res.status(400).json({
           message: "User not successful created",
