5e3a54bc23c92da72d47a06dc6cfc1e4c89dcca7
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 16:05:41 -0500

    Update accountController.js

--- LOGIN-API/Controllers/accountController.js
+++ LOGIN-API/Controllers/accountController.js
@@ -12,15 +12,6 @@ exports.register = async (req, res, next) => {
     return res.status(400).json({ message: "Password less than 6 characters" })
   }
   try {
-    await User.create({username,password,}).then(user =>res.status(200).json({  message: "User successfully created",user,
-      })
-    )
-  } catch (err) {
-    res.status(401).json({
-      message: "User not successful created",
-      error: error.mesage,
-    })
-}
   bcrypt.hash(password, 10).then(async (hash) => {
     await User.create({
       username,
