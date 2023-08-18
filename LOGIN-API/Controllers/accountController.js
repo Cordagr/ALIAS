0bb4653fd6e74d0b17b1155b02772703084d47c8
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 15:16:31 -0500

    Update accountController.js

--- LOGIN-API/Controllers/accountController.js
+++ LOGIN-API/Controllers/accountController.js
@@ -1,79 +1,23 @@
-// accountController.js
-// Import user model
-account = require('../models/accountModel');
-// Handle index actions
-exports.index = function (req, res) {
-    account.get(function (err, users) {
-        if (err) {
-            res.json({
-                status: "error",
-                message: err,
-            });
-        }
-        res.json({
-            status: "success",
-            message: "Accounts retrieved successfully",
-            data: users
-        });
-    });
-};
-// Handle create user actions
-exports.new = function (req, res) {
-    var user = new account();
-   
-   
-   
-    user.username = req.body.username ? req.body.username : user.username;
-    user.password = req.body.password ? req.body.password : user.password;
-// save the user and check for errors
-    user.save(function (err) {
-        // if (err)
-        //     res.json(err);
-        res.json({
-            message: 'New user created!',
-            data: user
-        });
-    });
-};
-// Handles viewing user information
-exports.view = function (req, res) {
-    account.findById(req.params.account_id, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json({
-            message: 'Account details loading..',
-            data: user
-        });
-    });
-};
-// Handle update user info
-exports.update = function (req, res) {
-account.findById(req.params.account_id, function (err, user) {
-        if (err)
-            res.send(err);
-            user.username = req.body.username ? req.body.username : user.username;
-            user.password = req.body.password ? req.body.password : user.password;
-// save the user and check for errors
-        user.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'Account Info updated',
-                data: user
-            });
-        });
-    });
-};
-// Handle delete user
-exports.delete = function (req, res) {
-    account.remove({
-        _id: req.params.account_id
-    }, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json({
-            status: "success",
-            message: 'Account deleted'
-        });
-    });
-};
+// auth.js
+exports.register = async (req, res, next) => {
+  const { username, password } = req.body
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password less than 6 characters" })
+  }
+  try {
+    await User.create({
+      username,
+      password,
+    }).then(user =>
+      res.status(200).json({
+        message: "User successfully created",
+        user,
+      })
+    )
+  } catch (err) {
+    res.status(401).json({
+      message: "User not successful created",
+      error: error.mesage,
+    })
+  }
+}
