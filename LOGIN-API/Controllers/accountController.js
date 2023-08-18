c987aadfba9dc671b19da5c3be5b7adb4abdab54
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 15:38:39 -0500

    Update accountController.js

--- LOGIN-API/Controllers/accountController.js
+++ LOGIN-API/Controllers/accountController.js
@@ -73,9 +73,21 @@ exports.update = async (req, res, next) => {
             .json({ message: "An error occurred", error: error.message });
         });
 
+//delete user//
 
-
-
+exports.deleteUser = async (req, res, next) => {
+  const { id } = req.body
+  await User.findById(id)
+    .then(user => user.remove())
+    .then(user =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
+    .catch(error =>
+      res
+        .status(400)
+        .json({ message: "An error occurred", error: error.message })
+    )
+}
 
   
 }
