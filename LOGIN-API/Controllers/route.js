a0670e2209a4378f7f7cf5ba0263f7ecaea78eba
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 15:25:23 -0500

    Update route.js

--- LOGIN-API/Controllers/route.js
+++ LOGIN-API/Controllers/route.js
@@ -2,7 +2,5 @@ const express = require("express")
 const router = express.Router()
 const { register } = require("./auth")
 router.route("/register").post(register)
-module.exports = router
-
 router.route("/login").post(login);
 module.exports = router;
