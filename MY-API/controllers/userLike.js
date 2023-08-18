edac3fa32de79e130cf89c066b87863da9a08df9
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 12:33:07 -0500

    Update userLike.js

--- MY-API/controllers/userLike.js
+++ MY-API/controllers/userLike.js
@@ -4,4 +4,14 @@ const initApp=async()=>
     const contacts=await getDataFromDB();
     //renders data to page
     //add listeners
+  };
+document.addEventListener("DOMContentLoaded",initApp);
+const getDataFromDB=async()=>
+  {
+    const DataStream=await fetch
+      ("fill in");
   }
+const jsonData=await fill.json();
+return json.data;
+};
+
