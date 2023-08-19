cdc59b9bc30d77f6f9fa3cba5172d0a7536cd80b
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 19:33:08 -0500

    Create pushUser.js

---
+++ MY-API/services/pushUser.js
@@ -0,0 +1,18 @@
+
+function pushUser(username,id)
+{
+let messages=[];
+if(!username)
+{
+  messages.push("username does not exist!");
+}
+
+  if(!username && id.length>255)
+{
+    messages.push("username does not exist and id is too long!");
+}
+
+  
+}
+
+async
