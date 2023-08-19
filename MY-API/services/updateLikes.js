f85171e731ed29a168caf93c6eb499437e1363e2
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 19:47:31 -0500

    Update updateLikes.js

--- MY-API/services/updateLikes.js
+++ MY-API/services/updateLikes.js
@@ -1,1 +1,37 @@
+function updateLikes(user)
+{
+let messages=[];
+if(!user.username)
+{
+  messages.push("username does not exist!");
+}
 
+  if(!user.username && user.id.length>255)
+{
+    messages.push("username does not exist and id is too long!");
+}
+}
+
+async function create(user)
+{
+  pushUser(user);
+    const result = await db.query(
+    'INSERT INTO quote (quote, author) VALUES (?, ?)', 
+    [user.username,user.id]
+  );
+
+  let message = 'Error in creating quote';
+
+  if (result.affectedRows) {
+    message = 'User created successfully!';
+  }
+
+  return {message};
+}
+
+module.exports = {
+  getMultiple,
+  create
+}
+
+}
