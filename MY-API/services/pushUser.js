fd0fc81ec7cf6f9e3388ae644467078dac03bcfb
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-08-18 19:37:35 -0500

    Update pushUser.js
    Added a way of exporting user and will be used within the CONTROLLERS in ("LOGIN-API")

--- MY-API/services/pushUser.js
+++ MY-API/services/pushUser.js
@@ -1,18 +1,38 @@
 
-function pushUser(username,id)
+function pushUser(user)
 {
 let messages=[];
-if(!username)
+if(!user.username)
 {
   messages.push("username does not exist!");
 }
 
-  if(!username && id.length>255)
+  if(!user.username && user.id.length>255)
 {
     messages.push("username does not exist and id is too long!");
 }
-
-  
 }
 
-async
+async function create(user)
+{
+  pushUser(user);
+    const result = await db.query(
+    'INSERT INTO quote (quote, author) VALUES (?, ?)', 
+    [user.username,user.id]
+  );
+
+  let message = 'Error in creating quote';
+
+  if (result.affectedRows) {
+    message = 'User created successfully!';
+  }
+
+  return {message};
+}
+
+module.exports = {
+  getMultiple,
+  create
+}
+
+}
