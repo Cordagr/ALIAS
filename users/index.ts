03e4ab80234f70b631fc5b20307e95357a09f20d
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-09-08 12:51:52 -0500

    Create [user.id].ts
    Adds the ability to find specific user through prismadb query and API request

---
+++ users/index.ts
@@ -0,0 +1,32 @@
+import {NextApiRequest, NextApiResponse}
+import prisma from '@/libs/prismadb'
+
+export default async function handler (
+req:NextApiRequest
+res:NextApiResponse
+)
+{
+  if(req.method≠ 'Get')
+  {
+    return res.status.end(405);
+  }
+  try{const {userID}=req.query;
+    if(!userID||typeof userID≠'string')
+      {
+        throw new Error ('Invalid ID');
+      }
+ const existingUser=await prisma.user.findUnique({
+   where:
+    id:UserID
+ }
+ });                                      
+  
+  
+}catch(error)
+  {
+    console.log(error);
+    return res.status(400).end;
+  }
+
+  
+}
