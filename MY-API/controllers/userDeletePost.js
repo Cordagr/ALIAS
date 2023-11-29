c7a032620eee82692ccddb9c7d634b28f48b0f0c
Cordagr <113870656+Cordagr@users.noreply.github.com>
2023-11-29 14:36:29 -0600

    Update userDeletePost.js

--- MY-API/controllers/userDeletePost.js
+++ MY-API/controllers/userDeletePost.js
@@ -1,1 +1,21 @@
 
+userDeletePost('/user-delete-post',url, async(req,res)=>
+{
+try(!req.files)
+  {
+    status: false,
+    message: 'No post was deleted' 
+  }
+
+  
+  
+  const jwt = require("jsonwebtoken");
+const signingCertificate = '-----BEGIN CERTIFICATION-----'
+---END CERTIFICATE---
+car claims = jwt.verify(token, signingCertificate)
+if(claims.exp && claims.exp <=new Date().getTime()/1000)
+throw "expired";
+var user = claims.sub
+}
+
+
